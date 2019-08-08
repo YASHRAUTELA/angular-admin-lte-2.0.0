1726f4c02c35f2726b44da6f4df5379e4077014c
Yashwant Rautela <yashrautela1@gmail.com>
2019-08-08 18:08:27 +0530

    admin-guard, super-admin-guard, login guard added and admin-common module added with common components

---
+++ src/app/module/admin-common/admin-common.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile/profile.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+
+@NgModule({
+  declarations: [ProfileComponent, ChangePasswordComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminCommonModule { }
